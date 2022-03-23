b391b279466459f0ed5810f647bf243ee8a2b8ba
Gilian <giliankeulens@happyhorizon.com>
2022-03-23 15:13:22 +0100

    Added settings for all HTML templates for the new order process component.

---
+++ GeeksCoreLibrary/Components/OrderProcess/Models/OrderProcessAutomaticSettingsModel.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel;
+
+namespace GeeksCoreLibrary.Components.OrderProcess.Models
+{
+    internal class OrderProcessAutomaticSettingsModel
+    {
+        #region Tab layout properties
+
+        [DefaultValue(@"<h1>Bestelproces</h1>
+{progress}
+<div id='steps'>{steps}</div>")]
+        internal string Template { get; }
+
+        [DefaultValue(@"<h2>{title}</h2>
+<div id='step_{title:Seo}'>
+    {groups}
+</div>")]
+        internal string TemplateStep { get; }
+
+        [DefaultValue(@"<fieldset>
+    <legend>{title}</legend>
+    {fields}
+</fieldset>")]
+        internal string TemplateGroup { get; }
+
+        [DefaultValue(@"<div id='container_{formfieldid}' class='field-container'>
+    [if({formfieldlabel}!)]<label for='{formfieldid}'>{formfieldlabel}</label>[endif]
+    <input type='{formfieldinputtype}' id='{formfieldid}' name='{formfieldid}' placeholder='{formfieldplaceholder}' {required} pattern='{formfieldregexcheck}' value='{value}' />
+</div>")]
+        internal string TemplateInputField { get; }
+
+        [DefaultValue(@"<div id='container_{formfieldid}' class='field-container'>
+    [if({formfieldlabel}!)]<label>{formfieldlabel}</label>[endif]
+    {options}
+</div>")]
+        internal string TemplateRadioButtonField { get; }
+        
+        [DefaultValue(@"<label>
+    <input type='radio' id='{formfieldid}_{option}' name='{formfieldid}' value='{option}' {required} {checked} />
+    <span class='label'>{option}</span>
+</label>")]
+        internal string TemplateRadioButtonFieldOption { get; }
+        
+        [DefaultValue(@"<div id='container_{formfieldid}' class='field-container'>
+    [if({formfieldlabel}!)]<label for='{formfieldid}'>{formfieldlabel}</label>[endif]
+    <select id='{formfieldid}' name='{formfieldid}' {required}>
+        {options}
+    </select>
+</div>")]
+        internal string TemplateSelectField { get; }
+        
+        [DefaultValue(@"<option value='{formfieldid}_{option}' name='{formfieldid}' {required} {checked} /> <span class='label'>{option}</span></label>")]
+        internal string TemplateSelectFieldOption { get; }
+        
+        [DefaultValue(@"<div id='container_{formfieldid}' class='field-container'>
+    <label>
+        <input type='checkbox' id='{formfieldid}' name='{formfieldid}' {required} {checked} value='1' />
+        <span class='label'>{formfieldlabel}</span>
+    </label>
+</div>")]
+        internal string TemplateCheckboxField { get; }
+
+        [DefaultValue(@"<div id='progress'>
+    {steps}
+</div>")]
+        internal string TemplateProgress { get; }
+
+        [DefaultValue(@"<div class='progress-step' id='progressStep{number}'>
+    <span class='number'>{number}</span>
+    <span class='name'>{name}</span>
+</div>")]
+        internal string TemplateProgressStep { get; }
+
+        [DefaultValue(@"<label>
+    <input type='radio' id='paymentMethod_{id}' value='{id}' name='paymentMethod' />
+    <img class='logo' src='{logo}' alt='{title}' />
+    <span class='label'>{title}</span>
+</label>")]
+        internal string TemplatePaymentMethod { get; }
+
+        #endregion
+    }
+}
--- GeeksCoreLibrary/Components/OrderProcess/Models/OrderProcessCmsSettingsModel.cs
+++ GeeksCoreLibrary/Components/OrderProcess/Models/OrderProcessCmsSettingsModel.cs
@@ -9,6 +9,9 @@ namespace GeeksCoreLibrary.Components.OrderProcess.Models
         
         #region Tab DataSource properties
 
+        /// <summary>
+        /// The Wiser item ID of the order process that should be retrieved.
+        /// </summary>
         [CmsProperty(
             PrettyName = "Order process item ID",
             Description = "The Wiser item ID of the order process that should be retrieved.",
@@ -18,6 +21,190 @@ namespace GeeksCoreLibrary.Components.OrderProcess.Models
             DisplayOrder = 10
         )]
         public ulong OrderProcessId { get; set; }
+
+        #endregion
+
+        #region Tab layout properties
+        
+        /// <summary>
+        /// The main template.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template",
+            Description = "The main template.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic,PaymentMethods",
+            DisplayOrder = 10
+        )]
+        public string Template { get; set; }
+        
+        /// <summary>
+        /// The template for a step in the order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template step",
+            Description = "The template for a step in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 20
+        )]
+        public string TemplateStep { get; set; }
+        
+        /// <summary>
+        /// The template for a group of fields in the order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template group",
+            Description = "The template for a group of fields in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 30
+        )]
+        public string TemplateGroup { get; set; }
+        
+        /// <summary>
+        /// The template for a normal input field in the order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template input field",
+            Description = "The template for a normal input field in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 40
+        )]
+        public string TemplateInputField { get; set; }
+        
+        /// <summary>
+        /// The template for a radio button field in the order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template radio button field",
+            Description = "The template for a radio button field in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 50
+        )]
+        public string TemplateRadioButtonField { get; set; }
+        
+        /// <summary>
+        /// The template for a single option in a radio button field in the order process
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template radio button option",
+            Description = "The template for a single option in a radio button field in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 55
+        )]
+        public string TemplateRadioButtonFieldOption { get; set; }
+        
+        /// <summary>
+        /// The template for a select / combobox field in the order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template select field",
+            Description = "The template for a select / combobox field in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 60
+        )]
+        public string TemplateSelectField { get; set; }
+        
+        /// <summary>
+        /// The template for a single option in a select field in the order process
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template select option",
+            Description = "The template for a single option in a select field in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 65
+        )]
+        public string TemplateSelectFieldOption { get; set; }
+        
+        /// <summary>
+        /// The template for a checkbox field in the order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template checkbox field",
+            Description = "The template for a checkbox field in the order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 70
+        )]
+        public string TemplateCheckboxField { get; set; }
+        
+        /// <summary>
+        /// The template for showing the progress of the user in a multi step order process.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template progress",
+            Description = "The template for showing the progress of the user in a multi step order process.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 80
+        )]
+        public string TemplateProgress { get; set; }
+        
+        /// <summary>
+        /// The template for a single step for 'TemplateProgress'.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template progress step",
+            Description = "The template for a single step for 'TemplateProgress'.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic",
+            DisplayOrder = 80
+        )]
+        public string TemplateProgressStep { get; set; }
+        
+        /// <summary>
+        /// The template for a single payment method.
+        /// </summary>
+        [CmsProperty(
+            PrettyName = "Template payment method",
+            Description = "The template for a single payment method.",
+            DeveloperRemarks = "",
+            TabName = CmsAttributes.CmsTabName.Layout,
+            GroupName = CmsAttributes.CmsGroupName.Templates,
+            TextEditorType = CmsAttributes.CmsTextEditorType.HtmlEditor,
+            ComponentMode = "Automatic,PaymentMethods",
+            DisplayOrder = 90
+        )]
+        public string TemplatePaymentMethod { get; set; }
 
         #endregion
     }
