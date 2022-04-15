da01c17e6d8b9dae99c638e327a62c745f177695
Mark van Dijk <markvandijk@happyhorizon.com>
2022-04-15 11:01:22 +0200

    Expand explanation of file loading in order process.

--- GeeksCoreLibrary/Components/OrderProcess/Controllers/OrderProcessesController.cs
+++ GeeksCoreLibrary/Components/OrderProcess/Controllers/OrderProcessesController.cs
@@ -140,7 +140,7 @@ namespace GeeksCoreLibrary.Components.OrderProcess.Controllers
             {
                 contentToWrite.Append(await pagesService.GetGlobalHeader(url, javascriptTemplates, cssTemplates));
 
-                // Load (S)CSS file with the same name as the order process.
+                // Load (S)CSS and/or JavaScript files with the same name as the order process.
                 if (!String.IsNullOrWhiteSpace(orderProcessSettings.Title))
                 {
                     var cssTemplateId = await templatesService.GetTemplateIdFromNameAsync(orderProcessSettings.Title, TemplateTypes.Scss);
