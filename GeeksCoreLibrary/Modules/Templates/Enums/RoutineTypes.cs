e44e905d270bc830a06ca6ae737f02745fc6dd21
Gilian <giliankeulens@happyhorizon.com>
2022-10-07 17:47:36 +0200

    Templates - Added functionality for setting an URL regex for each HTML, Query and Routine template. Also added functionality for executing routines and showing the results as JSON.

---
+++ GeeksCoreLibrary/Modules/Templates/Enums/RoutineTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace GeeksCoreLibrary.Modules.Templates.Enums
+{
+    /// <summary>
+    /// The possible SQL routine types.
+    /// </summary>
+    public enum RoutineTypes
+    {
+        Unknown = 0,
+        /// <summary>
+        /// Function.
+        /// </summary>
+        Function = 1,
+        /// <summary>
+        /// Stored procedure.
+        /// </summary>
+        Procedure = 2
+    }
+}
