a3ed63fe5f953a70d8c348a8dfdf27ada711e3e7
Gilian <giliankeulens@happyhorizon.com>
2022-10-10 17:17:41 +0200

    Moved RoutineTypes enum from Wiser to GCL.

---
+++ GeeksCoreLibrary/Modules/Templates/Enums/RoutineTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace GeeksCoreLibrary.Modules.Templates.Enums;
+
+/// <summary>
+/// The possible SQL routine types.
+/// </summary>
+public enum RoutineTypes
+{
+    Unknown = 0,
+    /// <summary>
+    /// Function.
+    /// </summary>
+    Function = 1,
+    /// <summary>
+    /// Stored procedure.
+    /// </summary>
+    Procedure = 2
+}
