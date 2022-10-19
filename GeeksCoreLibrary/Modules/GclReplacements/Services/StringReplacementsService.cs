687084714860f05f8b23b4f0d3f21eb5e64ea07f
Hans <35447467+HansJuice@users.noreply.github.com>
2022-10-19 17:12:05 +0200

    Fixed an issue with default values (#202)
    The question has to come after the colon, otherwise errors would occur.

--- GeeksCoreLibrary/Modules/GclReplacements/Services/StringReplacementsService.cs
+++ GeeksCoreLibrary/Modules/GclReplacements/Services/StringReplacementsService.cs
@@ -781,11 +781,14 @@ namespace GeeksCoreLibrary.Modules.GclReplacements.Services
                 //Checks for default values
                 if (fieldName.Contains("?"))
                 {
-                    var questionMarkIndexOf = fieldName.LastIndexOf("?");
-                    var colonIndexOf = fieldName.LastIndexOf(":");
-                    var defaultValueWithQuestionMark = colonIndexOf == -1 ? fieldName.Substring(questionMarkIndexOf) : fieldName.Substring(questionMarkIndexOf, colonIndexOf);
-                    defaultValue = defaultValueWithQuestionMark.Remove(0, 1);
-                    fieldName = fieldName.Remove(questionMarkIndexOf, defaultValueWithQuestionMark.Length);
+                    var questionMarkIndexOf = fieldName.LastIndexOf("?", StringComparison.Ordinal);
+                    var colonIndexOf = fieldName.LastIndexOf(":", StringComparison.Ordinal);
+                    if (questionMarkIndexOf + 1 > colonIndexOf)
+                    {
+                        var defaultValueWithQuestionMark = colonIndexOf == -1 ? fieldName.Substring(questionMarkIndexOf) : fieldName.Substring(questionMarkIndexOf, colonIndexOf);
+                        defaultValue = defaultValueWithQuestionMark.Remove(0, 1);
+                        fieldName = fieldName.Remove(questionMarkIndexOf, defaultValueWithQuestionMark.Length);
+                    }
                 }
                 
                 // Colons that are escaped with a backslash are temporarily replaced with "~~COLON~~".
