585940e596f7cdbd0b3a3307ebedc4f94f9badd1
Hans <35447467+HansJuice@users.noreply.github.com>
2022-11-10 11:15:40 +0100

    Removed BarcodeLib and added ZXing.NET instead
    ZXing.NET doesn't rely on `System.Drawing.Common` (which is problematic for non-Windows operating systems) and supports generating QR codes. Also adds the `ZXing.Net.Bindings.Magick` package so it uses `Magick.NET` to generate images. The `Magick.NET-Q8-x64` library has also been updated to the newest version (from 11.3.0 to 12.2.1).

--- GeeksCoreLibrary/Modules/Barcodes/Services/CachedBarcodesService.cs
+++ GeeksCoreLibrary/Modules/Barcodes/Services/CachedBarcodesService.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 using System.IO;
-using BarcodeLib;
 using GeeksCoreLibrary.Core.Helpers;
+using GeeksCoreLibrary.Core.Models;
 using GeeksCoreLibrary.Modules.Barcodes.Interfaces;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using ZXing;
 
 namespace GeeksCoreLibrary.Modules.Barcodes.Services;
 
+/// <inheritdoc cref="IBarcodesService"/>
 public class CachedBarcodesService : IBarcodesService
 {
+    private readonly GclSettings gclSettings;
     private readonly IBarcodesService barcodesService;
     private readonly IWebHostEnvironment webHostEnvironment;
 
-    public CachedBarcodesService(IBarcodesService barcodesService, IWebHostEnvironment webHostEnvironment)
+    /// <summary>
+    /// Creates a new instance of <see cref="CachedBarcodesService"/>.
+    /// </summary>
+    public CachedBarcodesService(IOptions<GclSettings> gclSettings, IBarcodesService barcodesService, IWebHostEnvironment webHostEnvironment)
     {
+        this.gclSettings = gclSettings.Value;
         this.barcodesService = barcodesService;
         this.webHostEnvironment = webHostEnvironment;
     }
 
     /// <inheritdoc />
-    public byte[] GenerateBarcode(string input, TYPE type, int width, int height)
+    public byte[] GenerateBarcode(string input, BarcodeFormat format, int width, int height)
     {
         var basePath = FileSystemHelpers.GetContentFilesFolderPath(webHostEnvironment);
-        var filename = $"barcode_{input}.png";
+        var filename = $"barcode_{format:G}_{width}x{height}_{input}.png";
         var filePath = Path.Combine(basePath, filename);
 
         var file = new FileInfo(filePath);
-        if (file.Exists && file.LastWriteTime >= DateTime.Now.AddHours(-1))
+        if (file.Exists && DateTime.UtcNow.Subtract(file.LastWriteTimeUtc) <= gclSettings.DefaultItemFileCacheDuration)
         {
             return File.ReadAllBytes(filePath);
         }
 
         // Generate new barcode if it doesn't exist yet or if it's older than one hour.
-        var fileBytes = barcodesService.GenerateBarcode(input, type, width, height);
+        var fileBytes = barcodesService.GenerateBarcode(input, format, width, height);
         FileSystemHelpers.SaveFileToContentFilesFolder(webHostEnvironment, filename, fileBytes);
         return fileBytes;
     }
