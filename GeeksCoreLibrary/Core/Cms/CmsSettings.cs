75d10b147927af1e3d0827841f574651309511fa
Gilian <giliankeulens@happyhorizon.com>
2022-12-23 12:26:31 +0100

    Fixed wrong exlenation in property.

--- GeeksCoreLibrary/Core/Cms/CmsSettings.cs
+++ GeeksCoreLibrary/Core/Cms/CmsSettings.cs
@@ -142,7 +142,7 @@ namespace GeeksCoreLibrary.Core.Cms
         [CmsProperty(
             PrettyName = "Cache regex",
             Description = @"<p>The regular expression to use for deciding the unique cache key. The value of each named group will be added to the cache key or file name. This is useful for a main menu for example, to cache the main menu separately for each main category on the site, so that the selected item in the menu will always be correct, even with caching.</p>
-                            <p>Example: If you use the regex ""\/(?<page>[^\/]*)?(\/.*)?"". This regex has 3 named groups (category, subCategory and product). If the user would then open the URL ""/products/drinks/soda/coca-cola/"", then the key or file name for the cache would be ""dynamicContent_123_drinks_soda_coca-cola"". This way the component will be cached separately for each product on the website.</p>
+                            <p>Example: If you use the regex ""\/products\/(?&lt;category&gt;.*)\/(?&lt;subCategory&gt;.*)\/(?&lt;product&gt;.*)\/"". This regex has 3 named groups (category, subCategory and product). If the user would then open the URL ""/products/drinks/soda/cola/"", then the key or file name for the cache would be ""dynamicContent_123_drinks_soda_cola"". This way the component will be cached separately for each product on the website.</p>
                             <p>Example 2: If you use the regex ""\/(?&lt;page&gt;[^\/]*)?(\/.*)?"", then component will only be cached separately for the first segment of the URL. So there will be a separate cache for a product page and for a content page, but not separate for each individual product for example.</p>",
             DeveloperRemarks = "",
             TabName = CmsAttributes.CmsTabName.Developer,
