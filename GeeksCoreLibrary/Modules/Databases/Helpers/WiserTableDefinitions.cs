bfb4dacb693859f57f6e096f709c950a25e528c0
Gilian <giliankeulens@happyhorizon.com>
2022-12-29 14:12:35 +0100

    Added missing column to table wiser_login_log.

--- GeeksCoreLibrary/Modules/Databases/Helpers/WiserTableDefinitions.cs
+++ GeeksCoreLibrary/Modules/Databases/Helpers/WiserTableDefinitions.cs
@@ -645,13 +645,14 @@ namespace GeeksCoreLibrary.Modules.Databases.Helpers
             new WiserTableDefinitionModel
             {
                 Name = WiserTableNames.WiserLoginLog,
-                LastUpdate = new DateTime(2022, 7, 7),
+                LastUpdate = new DateTime(2022, 12, 29),
                 Columns = new List<ColumnSettingsModel>
                 {
                     new("id", MySqlDbType.UInt64, notNull: true, isPrimaryKey: true, autoIncrement: true),
                     new("user_id", MySqlDbType.UInt64, notNull: true),
                     new("time_active", MySqlDbType.Time, notNull: true, defaultValue: "00:00:00"),
-                    new("added_on", MySqlDbType.DateTime, notNull: true)
+                    new("added_on", MySqlDbType.DateTime, notNull: true),
+                    new("time_active_changed_on", MySqlDbType.DateTime, notNull: true)
                 },
                 Indexes = new List<IndexSettingsModel>
                 {
