1b6ae83be361be8f6ef1ae34375b4f4fe7ef464e
Mark van Dijk <markvandijk@happyhorizon.com>
2023-01-24 17:04:31 +0100

    Add FTPS and SFTP support.

---
+++ GeeksCoreLibrary/Modules/Ftps/Extensions/EncryptionModesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GeeksCoreLibrary.Modules.Ftps.Enums;
+using FluentFTP;
+
+namespace GeeksCoreLibrary.Modules.Ftps.Extensions;
+
+public static class EncryptionModesExtensions
+{
+    public static FtpEncryptionMode ConvertToFtpsEncryptionMode(this EncryptionModes encryptionMode)
+    {
+        switch (encryptionMode)
+        {
+            case EncryptionModes.Auto:
+                return FtpEncryptionMode.Auto;
+            case EncryptionModes.None:
+                return FtpEncryptionMode.None;
+            case EncryptionModes.Implicit:
+                return FtpEncryptionMode.Implicit;
+            case EncryptionModes.Explicit:
+                return FtpEncryptionMode.Explicit;
+            default:
+                throw new NotImplementedException($"Encryption mode '{encryptionMode}' is not yet implemented to convert to FTPS.");
+        }
+    }
+}
---
+++ GeeksCoreLibrary/Modules/Ftps/Handlers/FtpsHandler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using GeeksCoreLibrary.Modules.Ftps.Extensions;
+using GeeksCoreLibrary.Modules.Ftps.Interfaces;
+using FluentFTP;
+using FluentFTP.Helpers;
+using GeeksCoreLibrary.Modules.Ftps.Models;
+
+namespace GeeksCoreLibrary.Modules.Ftps.Handlers;
+
+public class FtpsHandler : IFtpHandler
+{
+    private AsyncFtpClient client;
+
+    /// <inheritdoc />
+    public async Task OpenConnectionAsync(FtpSettings ftpSettings)
+    {
+        client = new AsyncFtpClient(ftpSettings.Host, ftpSettings.User, ftpSettings.Password, ftpSettings.Port);
+        client.EncryptionMode = ftpSettings.EncryptionMode.ConvertToFtpsEncryptionMode();
+        client.ValidateAnyCertificate = ftpSettings.Host.StartsWith("localhost", StringComparison.OrdinalIgnoreCase);
+        client.DataConnectionType = ftpSettings.UsePassive ? FtpDataConnectionType.AutoPassive : FtpDataConnectionType.AutoActive;
+
+        await client.Connect();
+    }
+
+    /// <inheritdoc />
+    public Task CloseConnectionAsync()
+    {
+        client.Dispose();
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> UploadAsync(bool allFilesInFolder, string uploadPath, string fromPath)
+    {
+        if (!allFilesInFolder)
+        {
+            return (await client.UploadFile(fromPath, uploadPath, createRemoteDir: true)).IsSuccess();
+        }
+        
+        var ftpResults = await client.UploadDirectory(fromPath, uploadPath, existsMode: FtpRemoteExists.Overwrite);
+        return ftpResults.Any(ftpResult => ftpResult.IsSuccess);
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> UploadAsync(string uploadPath, byte[] fileBytes)
+    {
+        return (await client.UploadBytes(fileBytes, uploadPath, createRemoteDir: true)).IsSuccess();
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> DownloadAsync(bool allFilesInFolder, string downloadPath, string writePath)
+    {
+        if (!allFilesInFolder)
+        {
+            return (await client.DownloadFile(writePath, downloadPath)).IsSuccess();
+        }
+        
+        // Get the names of the files that need to be downloaded.
+        var filesToDownload = await GetFilesInFolderAsync(downloadPath);
+        if (!filesToDownload.Any())
+        {
+            return true;
+        }
+            
+        // Combine the name with the path to the folder.
+        for (var i = 0; i < filesToDownload.Count; i++)
+        {
+            filesToDownload[i] = Path.Combine(downloadPath, filesToDownload[i]);
+        }
+        
+        var downloadCount = await client.DownloadFiles(writePath, filesToDownload);
+        return downloadCount.Count == filesToDownload.Count;
+    }
+
+    /// <inheritdoc />
+    public async Task<List<string>> GetFilesInFolderAsync(string folderPath)
+    {
+        var listing = await client.GetListing(folderPath);
+
+        return listing.Select(file => file.Name).ToList();
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> DeleteFileAsync(bool allFilesInFolder, string filePath)
+    {
+        if (allFilesInFolder)
+        {
+            var files = await GetFilesInFolderAsync(filePath);
+            foreach (var file in files)
+            {
+                await client.DeleteFile(Path.Combine(filePath, file));
+            }
+
+            return true;
+        }
+
+        await client.DeleteFile(filePath);
+        return true;
+    }
+}
