6fb1ddf8dc4431a2da3dac128673cde9758254fa
Gilian <giliankeulens@happyhorizon.com>
2023-11-13 14:05:52 +0100

    Added some more extensions to pages that should not be redirected.

--- GeeksCoreLibrary/Core/Models/CoreConstants.cs
+++ GeeksCoreLibrary/Core/Models/CoreConstants.cs
@@ -2,8 +2,8 @@
 {
     public class CoreConstants
     {
-        public const string UrlsToSkipForMiddlewaresRegex = @"(\.jpe?g|\.gif|\.png|\.webp|\.svg|\.bmp|\.tif|\.ico|\.woff2?|\.css|\.js|\.[gj]cl|\.webmanifest)(?:\?.*)?$";
-        
+        public const string UrlsToSkipForMiddlewaresRegex = @"(\.jpe?g|\.gif|\.png|\.webp|\.svg|\.bmp|\.tif|\.ico|\.woff2?|\.s?css|\.js|\.[gj]cl|\.webmanifest|\.ttf)(?:\?.*)?$";
+
         public const string SeoTitlePropertyName = "title_seo";
 
         internal const string RolesDataCachingKey = "GCLRoles";
