1178b90ed2b51908783a9eca8d30b6bec172b50f
Hans <35447467+HansJuice@users.noreply.github.com>
2024-02-02 17:33:35 +0100

    Added Lua script interpreter
    A service that allows to run Lua scripts. For now, it only works with functions.

---
+++ GeeksCoreLibrary/Modules/ScriptInterpreters/Lua/Interfaces/ILuaService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NLua;
+
+namespace GeeksCoreLibrary.Modules.ScriptInterpreters.Lua.Interfaces;
+
+public interface ILuaService : IDisposable
+{
+    /// <summary>
+    /// Create a function from a script.
+    /// </summary>
+    /// <param name="script">The Lua script that will create the function.</param>
+    /// <param name="functionName">The name of the function that the script will create.</param>
+    /// <returns>The <see cref="LuaFunction"/> that was created.</returns>
+    /// <exception cref="ArgumentNullException">If either <paramref name="script"/> or <paramref name="functionName"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">If the name passed in <paramref name="functionName"/> can not be found in the script.</exception>
+    LuaFunction CreateFunction(string script, string functionName);
+
+    LuaFunction GetFunction(string functionName);
+
+    object[] CallFunction(LuaFunction function, params object[] args);
+
+    object[] CallFunction(string functionName, params object[] args);
+}
---
+++ GeeksCoreLibrary/Modules/ScriptInterpreters/Lua/Services/LuaService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using GeeksCoreLibrary.Core.DependencyInjection.Interfaces;
+using GeeksCoreLibrary.Modules.ScriptInterpreters.Lua.Interfaces;
+using NLua;
+
+namespace GeeksCoreLibrary.Modules.ScriptInterpreters.Lua.Services;
+
+public class LuaService : ILuaService, ITransientService
+{
+    private NLua.Lua state = new();
+
+    public LuaFunction CreateFunction(string script, string functionName)
+    {
+        state.DoString(script);
+        if (state[functionName] is not LuaFunction scriptFunction) throw new ArgumentOutOfRangeException(nameof(functionName), $"The function {functionName} could not be found in the script.");
+
+        return scriptFunction;
+    }
+
+    public LuaFunction GetFunction(string functionName)
+    {
+        return state[functionName] as LuaFunction;
+    }
+
+    public object[] CallFunction(LuaFunction function, params object[] args)
+    {
+        if (function == null) throw new ArgumentNullException(nameof(function));
+
+        return function.Call(args);
+    }
+
+    public object[] CallFunction(string functionName, params object[] args)
+    {
+        if (state[functionName] is not LuaFunction scriptFunction) throw new ArgumentOutOfRangeException(nameof(functionName), $"The function {functionName} could not be found in the state.");
+        return scriptFunction.Call(args);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposing) return;
+
+        state?.Dispose();
+        state = null;
+    }
+}
