d782b4f03cfcfbb5c2538e66a8711e4976cb3227
Gilian <giliankeulens@happyhorizon.com>
2024-05-16 16:11:06 +0200

    Finished ReplacementsMediator Tests.

--- GeeksCoreLibrary.Tests/Core/Services/WiserItemsServiceTests.cs
+++ GeeksCoreLibrary.Tests/Core/Services/WiserItemsServiceTests.cs
@@ -13,70 +13,69 @@ using GeeksCoreLibrary.Core.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace GeeksCoreLibrary.Tests.Core.Services
+namespace GeeksCoreLibrary.Tests.Core.Services;
+
+public class WiserItemsServiceTests
 {
-    public class WiserItemsServiceTests
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private WiserItemsService wiserItemsService;
+    private Mock<IDatabaseConnection> databaseConnectionMock;
+    private Mock<IObjectsService> objectsServiceMock;
+    private Mock<IStringReplacementsService> stringReplacementsServiceMock;
+    private Mock<IDataSelectorsService> dataSelectorsServiceMock;
+    private Mock<IDatabaseHelpersService> databaseHelpersServiceMock;
+    private Mock<IOptions<GclSettings>> gclSettingsMock;
+    private Mock<ILogger<WiserItemsService>> loggerMock;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    [SetUp]
+    public void Setup()
     {
-        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        private WiserItemsService wiserItemsService;
-        private Mock<IDatabaseConnection> databaseConnectionMock;
-        private Mock<IObjectsService> objectsServiceMock;
-        private Mock<IStringReplacementsService> stringReplacementsServiceMock;
-        private Mock<IDataSelectorsService> dataSelectorsServiceMock;
-        private Mock<IDatabaseHelpersService> databaseHelpersServiceMock;
-        private Mock<IOptions<GclSettings>> gclSettingsMock;
-        private Mock<ILogger<WiserItemsService>> loggerMock;
-        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        // Create mocks.
+        databaseConnectionMock = new Mock<IDatabaseConnection>();
+        objectsServiceMock = new Mock<IObjectsService>();
+        stringReplacementsServiceMock = new Mock<IStringReplacementsService>();
+        dataSelectorsServiceMock = new Mock<IDataSelectorsService>();
+        databaseHelpersServiceMock = new Mock<IDatabaseHelpersService>();
+        gclSettingsMock = new Mock<IOptions<GclSettings>>();
+        loggerMock = new Mock<ILogger<WiserItemsService>>();
 
-        [SetUp]
-        public void Setup()
-        {
-            // Create mocks.
-            databaseConnectionMock = new Mock<IDatabaseConnection>();
-            objectsServiceMock = new Mock<IObjectsService>();
-            stringReplacementsServiceMock = new Mock<IStringReplacementsService>();
-            dataSelectorsServiceMock = new Mock<IDataSelectorsService>();
-            databaseHelpersServiceMock = new Mock<IDatabaseHelpersService>();
-            gclSettingsMock = new Mock<IOptions<GclSettings>>();
-            loggerMock = new Mock<ILogger<WiserItemsService>>();
+        // Create the service that we're testing.
+        wiserItemsService = new WiserItemsService(databaseConnectionMock.Object, objectsServiceMock.Object, stringReplacementsServiceMock.Object, dataSelectorsServiceMock.Object, databaseHelpersServiceMock.Object, gclSettingsMock.Object, loggerMock.Object);
 
-            // Create the service that we're testing.
-            wiserItemsService = new WiserItemsService(databaseConnectionMock.Object, objectsServiceMock.Object, stringReplacementsServiceMock.Object, dataSelectorsServiceMock.Object, databaseHelpersServiceMock.Object, gclSettingsMock.Object, loggerMock.Object);
+        // Setup the mocks.
+        databaseConnectionMock.Setup(x => x.GetAsync(It.Is<string>(query => query.Contains("SELECT permission.permissions")), It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<bool>()))
+            .ReturnsAsync(() =>
+            {
+                var dataTable = new DataTable();
+                dataTable.Columns.Add("permissions", typeof(int));
+                dataTable.Rows.Add(15);
+                return dataTable;
+            });
 
-            // Setup the mocks.
-            databaseConnectionMock.Setup(x => x.GetAsync(It.Is<string>(query => query.Contains("SELECT permission.permissions")), It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<bool>()))
-                .ReturnsAsync(() =>
-                {
-                    var dataTable = new DataTable();
-                    dataTable.Columns.Add("permissions", typeof(int));
-                    dataTable.Rows.Add(15);
-                    return dataTable;
-                });
+        databaseConnectionMock.Setup(x => x.GetAsync(It.Is<string>(query => query.Contains("SELECT readonly")), It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<bool>()))
+            .ReturnsAsync(() =>
+            {
+                var dataTable = new DataTable();
+                dataTable.Columns.Add("readonly", typeof(bool));
+                dataTable.Rows.Add(false);
+                return dataTable;
+            });
+    }
 
-            databaseConnectionMock.Setup(x => x.GetAsync(It.Is<string>(query => query.Contains("SELECT readonly")), It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<bool>()))
-                .ReturnsAsync(() =>
-                {
-                    var dataTable = new DataTable();
-                    dataTable.Columns.Add("readonly", typeof(bool));
-                    dataTable.Rows.Add(false);
-                    return dataTable;
-                });
-        }
+    [Test]
+    [TestCase(EntityActions.Read, AccessRights.Read)]
+    [TestCase(EntityActions.Create, AccessRights.Create)]
+    [TestCase(EntityActions.Delete, AccessRights.Delete)]
+    [TestCase(EntityActions.Update, AccessRights.Update)]
+    public async Task CheckIfEntityActionIsPossibleAsync_ItemAndPermissions_ReturnsSuccess(EntityActions action, AccessRights expectedFlag)
+    {
+        // Act
+        var actual = await wiserItemsService.CheckIfEntityActionIsPossibleAsync(1, action, 2, null, true, null);
 
-        [Test]
-        [TestCase(EntityActions.Read, AccessRights.Read)]
-        [TestCase(EntityActions.Create, AccessRights.Create)]
-        [TestCase(EntityActions.Delete, AccessRights.Delete)]
-        [TestCase(EntityActions.Update, AccessRights.Update)]
-        public async Task CheckIfEntityActionIsPossibleAsync_ItemAndPermissions_ReturnsSuccess(EntityActions action, AccessRights expectedFlag)
-        {
-            // Act
-            var actual = await wiserItemsService.CheckIfEntityActionIsPossibleAsync(1, action, 2, null, true, null);
-
-            // Assert
-            actual.ok.Should().BeTrue();
-            actual.errorMessage.Should().BeNullOrEmpty();
-            actual.permissions.Should().HaveFlag(expectedFlag, $"because we want to {expectedFlag.ToString()} an item");
-        }
+        // Assert
+        actual.ok.Should().BeTrue();
+        actual.errorMessage.Should().BeNullOrEmpty();
+        actual.permissions.Should().HaveFlag(expectedFlag, $"because we want to {expectedFlag.ToString()} an item");
     }
 }
