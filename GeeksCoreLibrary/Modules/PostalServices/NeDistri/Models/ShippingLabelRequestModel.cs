02bf0618f1c9826d9579ff15bba6983e9f0d3978
Marco Ketelaars <marcoketelaars@happyhorizon.com>
2024-08-30 17:09:36 +0200

    Add XML comments to ShippingLabelRequestModel

--- GeeksCoreLibrary/Modules/PostalServices/NeDistri/Models/ShippingLabelRequestModel.cs
+++ GeeksCoreLibrary/Modules/PostalServices/NeDistri/Models/ShippingLabelRequestModel.cs
@@ -6,15 +6,27 @@ namespace GeeksCoreLibrary.Modules.PostalServices.NeDistri.Models;
 public class ShippingLabelRequestModel
 {
     /// <summary>
-    /// 
+    /// Comma seperated list of order ids
     /// </summary>
     public string EncryptedOrderIds { get; set; }
     
+    /// <summary>
+    /// Type of label. This is the string that will get send as label type to NE Distriservice
+    /// </summary>
     public string LabelType { get; set; }
     
+    /// <summary>
+    /// Colli amount
+    /// </summary>
     public int ColliAmount { get; set; }
     
+    /// <summary>
+    /// User code to use in case a login has multiple users attached to it
+    /// </summary>
     public int? UserCode { get; set; }
 
+    /// <summary>
+    /// Type of order. This can be a normal shipment or a return shipment from the customer.
+    /// </summary>
     public OrderType OrderType { get; set; } = OrderType.Shipment;
 }
