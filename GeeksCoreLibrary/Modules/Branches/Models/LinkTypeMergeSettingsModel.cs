379da902d62626c33fab0e0dee4a13fdb6ec7074
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-10-17 16:06:25 +0200

    Add linktypes to mergesettingsmodule

---
+++ GeeksCoreLibrary/Modules/Branches/Models/LinkTypeMergeSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using GeeksCoreLibrary.Modules.Branches.Enumerations;
+
+namespace GeeksCoreLibrary.Modules.Branches.Models
+{
+    /// <inheritdoc />
+    public class LinkTypeMergeSettingsModel : ObjectMergeSettingsModel
+    {
+        /// <summary>
+        /// Gets or sets the link type to merge.
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
