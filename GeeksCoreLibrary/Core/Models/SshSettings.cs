98c4a01b1a23895bd53242e942e9dacf76771525
Hans <35447467+HansHappyHorizon@users.noreply.github.com>
2024-11-11 12:18:10 +0100

    SSH settings private key bytes (#689)
    Added option to set the private key as a byte array instead of a file
    path.
    
    Co-authored-by: Hans <35447467+HansJuice@users.noreply.github.com>

--- GeeksCoreLibrary/Core/Models/SshSettings.cs
+++ GeeksCoreLibrary/Core/Models/SshSettings.cs
@@ -6,22 +6,22 @@
 public class SshSettings
 {
     /// <summary>
-    /// The host of the SSH tunnel/server.
+    /// Gets or sets the host of the SSH tunnel/server.
     /// </summary>
     public string Host { get; set; }
 
     /// <summary>
-    /// The port number to use for the SSH connection, default is 22.
+    /// Gets or sets the port number to use for the SSH connection, default is 22.
     /// </summary>
     public int Port { get; set; } = 22;
 
     /// <summary>
-    /// The username for the SSH connection.
+    /// Gets or sets the username for the SSH connection.
     /// </summary>
     public string Username { get; set; }
 
     /// <summary>
-    /// The password for the SSH connection, if applicable.
+    /// Gets or sets the password for the SSH connection, if applicable.
     /// Check if your SSH servers uses a password. Some uses a private key with passphrase without a password,
     /// in that case you should use leave this empty and use the PrivateKeyPath and PrivateKeyPassphrase properties instead.
     /// It's also possible to use both a password and a private key + passphrase.
@@ -29,17 +29,24 @@ public class SshSettings
     public string Password { get; set; }
 
     /// <summary>
-    /// The path to the private key file for the SSH connection, if applicable.
+    /// Gets or sets the path to the private key file for the SSH connection, if applicable.
+    /// If both <c>PrivateKeyPath</c> and <c>PrivateKeyBytes</c> are set, <c>PrivateKeyPath</c> will be used.
     /// </summary>
     public string PrivateKeyPath { get; set; }
 
     /// <summary>
-    /// The passphrase for the private key file, if applicable.
+    /// Gets or sets the private key bytes for the SSH connection, if applicable.
+    /// If both <c>PrivateKeyPath</c> and <c>PrivateKeyBytes</c> are set, <c>PrivateKeyPath</c> will be used.
+    /// </summary>
+    public byte[] PrivateKeyBytes { get; set; }
+
+    /// <summary>
+    /// Gets or sets the passphrase for the private key file, if applicable.
     /// </summary>
     public string PrivateKeyPassphrase { get; set; }
 
     /// <summary>
-    /// The host key fingerprint to expect from the SSH server. This can be left empty to not validate the host fingerprint,
+    /// Gets or sets the host key fingerprint to expect from the SSH server. This can be left empty to not validate the host fingerprint,
     /// but that is not recommended as it can be a security risk. If you don't validate this, then man-in-the-middle attacks are possible.
     /// You can get the fingerprint from the administrator of the host, or by connecting to the host and reading the fingerprint from the connection.
     /// The fingerprint is a SHA256 hash and is always the same for each host.
