300a0ff0aa0d784add967ad1633f26c540179877
Gilian <35449373+GilianJuice@users.noreply.github.com>
2025-01-20 15:31:58 +0100

    Background service improvements (#745)
    * Added support for running more than one background service and did some code cleanup.
    
    * Small fix after rebase.

--- GeeksCoreLibrary/Core/Services/ScopedBackgroundService.cs
+++ GeeksCoreLibrary/Core/Services/ScopedBackgroundService.cs
@@ -1,26 +1,19 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using GeeksCoreLibrary.Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GeeksCoreLibrary.Core.Services;
 
 /// <summary>
-/// Hosting services that inherit this service class will run in a scoped setting instead of singleton, allowing scoped services (such as DatabaseConnection)
-/// to be used.
+/// Hosting services that inherit this service class will run in a scoped setting instead of singleton,
+/// allowing scoped services (such as DatabaseConnection) to be used.
 /// </summary>
-public class ScopedBackgroundService : BackgroundService
+public class ScopedBackgroundService(IServiceProvider serviceProvider) : BackgroundService
 {
-    private readonly IServiceProvider serviceProvider;
-
-    public ScopedBackgroundService(ILogger<ScopedBackgroundService> logger, IServiceProvider serviceProvider)
-    {
-        this.serviceProvider = serviceProvider;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await DoWorkAsync(stoppingToken);
@@ -29,9 +22,9 @@ public class ScopedBackgroundService : BackgroundService
     private async Task DoWorkAsync(CancellationToken stoppingToken)
     {
         using var scope = serviceProvider.CreateScope();
-        var scopedProcessingService = scope.ServiceProvider.GetRequiredService<IScopedProcessingService>();
+        var scopedProcessingServices = scope.ServiceProvider.GetServices<IScopedProcessingService>();
 
-        await scopedProcessingService.DoWorkAsync(stoppingToken);
+        await Task.WhenAll(scopedProcessingServices.Select(scopedProcessingService => scopedProcessingService.DoWorkAsync(stoppingToken)).ToArray());
     }
 
     public override async Task StopAsync(CancellationToken stoppingToken)
