651fdccfdae7f01db8648862fc1b06a42ae504fd
Gilian <giliankeulens@happyhorizon.com>
2025-01-20 17:02:55 +0100

    Indent fix.

--- GeeksCoreLibrary/Core/Extensions/ConfigurationServiceCollectionExtensions.cs
+++ GeeksCoreLibrary/Core/Extensions/ConfigurationServiceCollectionExtensions.cs
@@ -284,7 +284,7 @@ public static class ConfigurationServiceCollectionExtensions
 
         // Enable caching.
         services.AddLazyCache();
-            services.AddDistributedMemoryCache();
+        services.AddDistributedMemoryCache();
 
         // Enable session.
         if (isWeb)
