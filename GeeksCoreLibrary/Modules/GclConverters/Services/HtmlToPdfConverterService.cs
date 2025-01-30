6ffb39e0f8b00f2e5a64b3530000eb0d2c85a0cf
Gilian <giliankeulens@happyhorizon.com>
2025-01-30 17:03:43 +0100

    Refactored the code for getting Wiser files from database:  * There was a ton of duplicate code, which I improved now.  * For some reason, the `ItemFilesService` would always write files to the hard disk, even if caching is disabled. But if caching is disabled, those files were never used. This has been refactored so that only the `CachedItemFilesService` writes files to disk.  * Some other minor refactorings/improvements.

--- GeeksCoreLibrary/Modules/GclConverters/Services/HtmlToPdfConverterService.cs
+++ GeeksCoreLibrary/Modules/GclConverters/Services/HtmlToPdfConverterService.cs
@@ -382,7 +382,7 @@ public class HtmlToPdfConverterService : IHtmlToPdfConverterService, IScopedServ
             return null;
         }
 
-        var filePath = await FileSystemHelpers.SaveToPublicFilesDirectoryAsync(webHostEnvironment, filename, content);
+        var filePath = await FileSystemHelpers.SaveFileToCacheAsync(webHostEnvironment, filename, content);
         return filePath?.Replace(webHostEnvironment.WebRootPath, "")?.Replace(@"\", "/");
     }
 }
