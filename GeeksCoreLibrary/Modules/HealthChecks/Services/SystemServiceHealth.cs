523fbcdc5ca3f42b6d52ba9b693c9de3571cdc33
ObidaHamd <obidahamd@happyhorizon.com>
2025-03-28 09:52:01 +0100

    Small changes

--- GeeksCoreLibrary/Modules/HealthChecks/Services/SystemServiceHealth.cs
+++ GeeksCoreLibrary/Modules/HealthChecks/Services/SystemServiceHealth.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
-using System.Diagnostics;
+
 
 using GeeksCoreLibrary.Modules.HealthChecks.Services;
  
