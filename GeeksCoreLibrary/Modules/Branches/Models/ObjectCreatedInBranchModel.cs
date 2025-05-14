ea321191f604be131971088356b1744ea8c45bc7
Gilian <giliankeulens@happyhorizon.com>
2025-05-14 12:28:12 +0200

    Added some new properties to `ObjectCreatedInBranchModel` that are needed in the WTS.

--- GeeksCoreLibrary/Modules/Branches/Models/ObjectCreatedInBranchModel.cs
+++ GeeksCoreLibrary/Modules/Branches/Models/ObjectCreatedInBranchModel.cs
@@ -1,4 +1,7 @@
-﻿namespace GeeksCoreLibrary.Modules.Branches.Models;
+﻿using System.Collections.Generic;
+using GeeksCoreLibrary.Modules.Databases.Models;
+
+namespace GeeksCoreLibrary.Modules.Branches.Models;
 
 /// <summary>
 /// A simple model for keeping track of objects that have been created and deleted in the branch.
@@ -6,9 +9,14 @@
 public class ObjectCreatedInBranchModel
 {
     /// <summary>
-    /// Gets or sets the ID of the object.
+    /// Gets or sets the ID of the object in the branch database.
     /// </summary>
     public string ObjectId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ID of the object in the production database.
+    /// </summary>
+    public string ObjectIdMapped { get; set; }
 
     /// <summary>
     /// Gets or sets the name of the table that contains the object.
@@ -21,7 +29,13 @@ public class ObjectCreatedInBranchModel
     public bool AlsoDeleted { get; set; }
 
     /// <summary>
-    /// Gets or sets whether or not the item was undeleted.
+    /// Gets or sets whether the item was undeleted.
     /// </summary>
     public bool AlsoUndeleted { get; set; }
+
+    /// <summary>
+    /// The columns that we generated temporary values for when creating the object in production.
+    /// We need to keep track of these columns, so that we can remove the temporary values after we're done merging.
+    /// </summary>
+    public List<ColumnSettingsModel> ColumnsWithTemporaryValues { get; set; } = [];
 }
